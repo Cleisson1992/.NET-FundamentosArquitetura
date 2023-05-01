85e9a243ed4cb1c2e15b40f33dad0bdd310c826f
Cleisson <cleissonvasconcelos1992@hotmail.com>
2023-05-01 14:10:04 -0300

    Validando retangulo.

--- SOLID/3 - LSP/LSP.Solucao/Retangulo.cs
+++ SOLID/3 - LSP/LSP.Solucao/Retangulo.cs
@@ -1,11 +1,14 @@
-﻿namespace SOLID.LSP.Solucao
+﻿using System;
+
+namespace SOLID.LSP.Solucao
 {
     public class Retangulo : Paralelogramo
     {
         public Retangulo(int altura, int largura)
             :base(altura,largura)
         {
-
+            if (largura == altura)
+                throw new ArgumentException("Os dois lados do retangulo precisam ser diferentes");
         }
     }
 }
